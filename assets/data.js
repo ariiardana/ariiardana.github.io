356e0b921391f205669926c1c6e033ab0df3f70b
Ari Ardana <62498753+ariiardana@users.noreply.github.com>
2020-05-19 00:21:28 +0700

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -3,17 +3,17 @@ const projek = [{
         sc: 'https://github.com/ariiardana/ariiardana.github.io/somebody',
         web: 'https://ariiardana.github.io/somebody',
         desc: 'Buat Nembak Ceue :v',
-        dibuat: 'HTML, CSS, Javascript',
+        dibuat: 'html, css, javascript',
         status: 'selesai',
         foto: './assets//img/img1.png'
     },
-                {
+    {
         nama: 'Info Covid-19',
         sc: 'https://github.com/ariiardana/ariiardana.github.io/covid',
         web: 'https://ariiardana.github.io/covid',
         desc: 'Info Covid-19 Real-Time',
-        dibuat: 'HTML, CSS, Javascript',
+        dibuat: 'html, css, javascript',
         status: 'selesai',
-        foto: './assets//img/img2.png'
+        foto: './assets//img/img1.png'
     },
 ]
